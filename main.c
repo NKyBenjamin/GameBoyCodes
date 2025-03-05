36e9399021a5e8ffba6cbc8e4ee6a877adc441c1
NKyBenjamin <72364799+NKyBenjamin@users.noreply.github.com>
2025-03-05 17:54:13 -0500

    Boom, added my name!

--- main.c
+++ main.c
@@ -1,11 +1,11 @@
 #include <gb/gb.h>
 #include <stdio.h>
-#include "XOSprites.c"
+#include "MyName.c"
 
 void main() {
     UINT8 currentspriteindex = 0;
 
-    set_sprite_data(0, 2, XOSprites);
+    set_sprite_data(0, 3, NKySprites);
     set_sprite_tile(0, 0);
     move_sprite(0, 88, 78);
     SHOW_SPRITES;
