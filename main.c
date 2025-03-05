c59b8a15dcb5aff145f9835b7971e4aec989c729
NKyBenjamin <72364799+NKyBenjamin@users.noreply.github.com>
2025-03-05 17:43:47 -0500

    Added more control over sprites!

--- main.c
+++ main.c
@@ -1,12 +1,22 @@
 #include <gb/gb.h>
 #include <stdio.h>
-#include "SmileDemo.c"
+#include "XOSprites.c"
 
 void main() {
-    set_sprite_data(0, 2, Smiles);
+    UINT8 currentspriteindex = 0;
+
+    set_sprite_data(0, 2, XOSprites);
     set_sprite_tile(0, 0);
     move_sprite(0, 88, 78);
     SHOW_SPRITES;
+
+    while(1) {
+        currentspriteindex = !currentspriteindex;
+        set_sprite_tile(0, currentspriteindex);
+        wait_vbl_done();
+        delay(500);
+        
+    }
 }
     
 
